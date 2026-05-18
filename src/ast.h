53e20857e968a4b47823fa17540ac97ad99722e0
atharva-paik <paikatharva@gmail.com>
2026-05-18 23:37:41 +0530

    Updated ast.h

--- src/ast.h
+++ src/ast.h
@@ -1,57 +1,15 @@
-//Implemented Abstract Syntax Tree logic
 #pragma once
-#include <iostream>
 #include <string>
 #include <memory>
 #include <vector>
+#include <iostream>
 #include "lexer.h"
 
-inline const char* tokenTypeToString(TokenType type) {
-    switch (type) {
-        case PLUS: return "PLUS";
-        case MINUS: return "MINUS";
-        case STAR: return "STAR";
-        case SLASH: return "SLASH";
-        case ASSIGN: return "ASSIGN";
-        case EQUAL_EQUAL: return "EQUAL_EQUAL";
-        case LESS: return "LESS";
-        case GREATER: return "GREATER";
-        case LESS_EQUAL: return "LESS_EQUAL";
-        case GREATER_EQUAL: return "GREATER_EQUAL";
-        case BOOL_TRUE: return "BOOL_TRUE";
-        case BOOL_FALSE: return "BOOL_FALSE";
-        case BANG_EQUAL: return "BANG_EQUAL";
-        case LEFT_PAREN: return "LEFT_PAREN";
-        case RIGHT_PAREN: return "RIGHT_PAREN";
-        case LEFT_BRACE: return "LEFT_BRACE";
-        case RIGHT_BRACE: return "RIGHT_BRACE";
-        case SEMICOLON: return "SEMICOLON";
-        case NUMBER: return "NUMBER";
-        case IDENTIFIER: return "IDENTIFIER";
-        case LET: return "LET";
-        case IF: return "IF";
-        case ELSE: return "ELSE";
-        case WHILE: return "WHILE";
-        case PRINT: return "PRINT";
-        case INPUT: return "INPUT";
-        case EOF_TOKEN: return "EOF_TOKEN";
-        default: return "UNKNOWN_TOKEN";
-    }
-}
-
-struct ASTNode {
-    virtual ~ASTNode() = default;
-    virtual void print(int depth = 0) const = 0;
-};
+struct ASTNode { virtual ~ASTNode() = default; };
 
 struct NumberNode : public ASTNode {
     int value;
     NumberNode(int val) { value = val; }
-
-    void print(int depth = 0) const override {
-        std::string indent(depth * 2, ' ');
-        std::cout << indent << "NumberNode: " << value << "\n";
-    }
 };
 
 struct BinaryOpNode : public ASTNode {
@@ -61,12 +19,13 @@ struct BinaryOpNode : public ASTNode {
     BinaryOpNode(TokenType operation, std::shared_ptr<ASTNode> leftChild, std::shared_ptr<ASTNode> rightChild) {
         op = operation; left = leftChild; right = rightChild;
     }
+};
 
-    void print(int depth = 0) const override {
-        std::string indent(depth * 2, ' ');
-        std::cout << indent << "BinaryOpNode: " << tokenTypeToString(op) << "\n";
-        if (left) left->print(depth + 1);
-        if (right) right->print(depth + 1);
+struct UnaryOpNode : public ASTNode {
+    TokenType op;
+    std::shared_ptr<ASTNode> operand;
+    UnaryOpNode(TokenType operation, std::shared_ptr<ASTNode> expr) {
+        op = operation; operand = expr;
     }
 };
 
@@ -74,22 +33,11 @@ struct VarDeclNode : public ASTNode {
     std::string varName;
     std::shared_ptr<ASTNode> value; 
     VarDeclNode(std::string name, std::shared_ptr<ASTNode> val) { varName = name; value = val; }
-
-    void print(int depth = 0) const override {
-        std::string indent(depth * 2, ' ');
-        std::cout << indent << "VarDeclNode: " << varName << "\n";
-        if (value) value->print(depth + 1);
-    }
 };
 
 struct VariableNode : public ASTNode {
     std::string varName;
     VariableNode(std::string name) { varName = name; }
-
-    void print(int depth = 0) const override {
-        std::string indent(depth * 2, ' ');
-        std::cout << indent << "VariableNode: " << varName << "\n";
-    }
 };
 
 
@@ -97,60 +45,28 @@ struct AssignNode : public ASTNode {
     std::string varName;
     std::shared_ptr<ASTNode> value;
     AssignNode(std::string name, std::shared_ptr<ASTNode> val) { varName = name; value = val; }
-
-    void print(int depth = 0) const override {
-        std::string indent(depth * 2, ' ');
-        std::cout << indent << "AssignNode: " << varName << "\n";
-        if (value) value->print(depth + 1);
-    }
 };
 
 struct BlockNode : public ASTNode {
     std::vector<std::shared_ptr<ASTNode>> statements;
     BlockNode(std::vector<std::shared_ptr<ASTNode>> stmts) { statements = stmts; }
-
-    void print(int depth = 0) const override {
-        std::string indent(depth * 2, ' ');
-        std::cout << indent << "BlockNode\n";
-        for (const auto& statement : statements) {
-            if (statement) statement->print(depth + 1);
-        }
-    }
 };
 
 struct WhileNode : public ASTNode {
     std::shared_ptr<ASTNode> condition;
     std::shared_ptr<ASTNode> body;
     WhileNode(std::shared_ptr<ASTNode> cond, std::shared_ptr<ASTNode> b) { condition = cond; body = b; }
-
-    void print(int depth = 0) const override {
-        std::string indent(depth * 2, ' ');
-        std::cout << indent << "WhileNode\n";
-        if (condition) condition->print(depth + 1);
-        if (body) body->print(depth + 1);
-    }
 };
 
 
 struct PrintNode : public ASTNode {
     std::shared_ptr<ASTNode> value;
     PrintNode(std::shared_ptr<ASTNode> val) { value = val; }
-
-    void print(int depth = 0) const override {
-        std::string indent(depth * 2, ' ');
-        std::cout << indent << "PrintNode\n";
-        if (value) value->print(depth + 1);
-    }
 };
 
 struct InputNode : public ASTNode {
     std::string varName;
     InputNode(std::string name) { varName = name; }
-
-    void print(int depth = 0) const override {
-        std::string indent(depth * 2, ' ');
-        std::cout << indent << "InputNode: " << varName << "\n";
-    }
 };
 
 struct IfNode : public ASTNode {
@@ -161,22 +77,124 @@ struct IfNode : public ASTNode {
     IfNode(std::shared_ptr<ASTNode> cond, std::shared_ptr<ASTNode> thenB, std::shared_ptr<ASTNode> elseB) {
         condition = cond; thenBranch = thenB; elseBranch = elseB;
     }
-
-    void print(int depth = 0) const override {
-        std::string indent(depth * 2, ' ');
-        std::cout << indent << "IfNode\n";
-        if (condition) condition->print(depth + 1);
-        if (thenBranch) thenBranch->print(depth + 1);
-        if (elseBranch) elseBranch->print(depth + 1);
-    }
 };
-
 struct BoolNode : public ASTNode {
     bool value;
     BoolNode(bool val) { value = val; }
-
-    void print(int depth = 0) const override {
-        std::string indent(depth * 2, ' ');
-        std::cout << indent << "BoolNode: " << (value ? "true" : "false") << "\n";
-    }
 };
+
+inline std::string tokenTypeToString(TokenType type) {
+    switch (type) {
+        case PLUS: return "+";
+        case MINUS: return "-";
+        case STAR: return "*";
+        case SLASH: return "/";
+        case ASSIGN: return "=";
+        case EQUAL_EQUAL: return "==";
+        case LESS: return "<";
+        case GREATER: return ">";
+        case LESS_EQUAL: return "<=";
+        case GREATER_EQUAL: return ">=";
+        case BANG_EQUAL: return "!=";
+        case BOOL_TRUE: return "true";
+        case BOOL_FALSE: return "false";
+        case LEFT_PAREN: return "(";
+        case RIGHT_PAREN: return ")";
+        case LEFT_BRACE: return "{";
+        case RIGHT_BRACE: return "}";
+        case SEMICOLON: return ";";
+        case NUMBER: return "NUMBER";
+        case IDENTIFIER: return "IDENTIFIER";
+        case LET: return "let";
+        case IF: return "if";
+        case ELSE: return "else";
+        case WHILE: return "while";
+        case PRINT: return "print";
+        case INPUT: return "input";
+        case EOF_TOKEN: return "EOF";
+        default: return "UNKNOWN";
+    }
+}
+
+inline void printIndent(int indent) {
+    for (int i = 0; i < indent; ++i) std::cout << "  ";
+}
+
+inline void printASTNode(const std::shared_ptr<ASTNode>& node, int indent = 0) {
+    if (!node) {
+        printIndent(indent);
+        std::cout << "<null>\n";
+        return;
+    }
+
+    if (auto numberNode = std::dynamic_pointer_cast<NumberNode>(node)) {
+        printIndent(indent);
+        std::cout << "Number(" << numberNode->value << ")\n";
+    } else if (auto boolNode = std::dynamic_pointer_cast<BoolNode>(node)) {
+        printIndent(indent);
+        std::cout << "Bool(" << (boolNode->value ? "true" : "false") << ")\n";
+    } else if (auto variableNode = std::dynamic_pointer_cast<VariableNode>(node)) {
+        printIndent(indent);
+        std::cout << "Variable(" << variableNode->varName << ")\n";
+    } else if (auto unaryNode = std::dynamic_pointer_cast<UnaryOpNode>(node)) {
+        printIndent(indent);
+        std::cout << "UnaryOp(" << tokenTypeToString(unaryNode->op) << ")\n";
+        printASTNode(unaryNode->operand, indent + 1);
+    } else if (auto binaryNode = std::dynamic_pointer_cast<BinaryOpNode>(node)) {
+        printIndent(indent);
+        std::cout << "BinaryOp(" << tokenTypeToString(binaryNode->op) << ")\n";
+        printASTNode(binaryNode->left, indent + 1);
+        printASTNode(binaryNode->right, indent + 1);
+    } else if (auto varDeclNode = std::dynamic_pointer_cast<VarDeclNode>(node)) {
+        printIndent(indent);
+        std::cout << "VarDecl(" << varDeclNode->varName << ")\n";
+        printASTNode(varDeclNode->value, indent + 1);
+    } else if (auto assignNode = std::dynamic_pointer_cast<AssignNode>(node)) {
+        printIndent(indent);
+        std::cout << "Assign(" << assignNode->varName << ")\n";
+        printASTNode(assignNode->value, indent + 1);
+    } else if (auto printNode = std::dynamic_pointer_cast<PrintNode>(node)) {
+        printIndent(indent);
+        std::cout << "Print\n";
+        printASTNode(printNode->value, indent + 1);
+    } else if (auto inputNode = std::dynamic_pointer_cast<InputNode>(node)) {
+        printIndent(indent);
+        std::cout << "Input(" << inputNode->varName << ")\n";
+    } else if (auto blockNode = std::dynamic_pointer_cast<BlockNode>(node)) {
+        printIndent(indent);
+        std::cout << "Block\n";
+        for (const auto& statement : blockNode->statements) printASTNode(statement, indent + 1);
+    } else if (auto whileNode = std::dynamic_pointer_cast<WhileNode>(node)) {
+        printIndent(indent);
+        std::cout << "While\n";
+        printIndent(indent + 1);
+        std::cout << "Condition\n";
+        printASTNode(whileNode->condition, indent + 2);
+        printIndent(indent + 1);
+        std::cout << "Body\n";
+        printASTNode(whileNode->body, indent + 2);
+    } else if (auto ifNode = std::dynamic_pointer_cast<IfNode>(node)) {
+        printIndent(indent);
+        std::cout << "If\n";
+        printIndent(indent + 1);
+        std::cout << "Condition\n";
+        printASTNode(ifNode->condition, indent + 2);
+        printIndent(indent + 1);
+        std::cout << "Then\n";
+        printASTNode(ifNode->thenBranch, indent + 2);
+        if (ifNode->elseBranch) {
+            printIndent(indent + 1);
+            std::cout << "Else\n";
+            printASTNode(ifNode->elseBranch, indent + 2);
+        }
+    } else {
+        printIndent(indent);
+        std::cout << "UnknownNode\n";
+    }
+}
+
+inline void printAST(const std::vector<std::shared_ptr<ASTNode>>& program) {
+    std::cout << "===== AST =====\n";
+    for (const auto& statement : program) printASTNode(statement, 0);
+    std::cout << "===============\n";
+}
